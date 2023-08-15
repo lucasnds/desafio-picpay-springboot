8cfed7faf8fa559925ed373a13496ed0a2f52131
Lucas Silva <lucasdoismile16@gmail.com>
2023-08-15 13:01:58 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User, Long > {
+
+    //O optional foi usado pq é opcional retornar um usuário, nem sempre vai acontecer
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}
